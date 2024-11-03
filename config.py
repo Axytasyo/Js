e14096b9ba7ac71c9ce1413bc94d271a6ba6aaec
Axytasyo <136109087+Axytasyo@users.noreply.github.com>
2024-11-04 02:40:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+API_ID = 27971506  # api id
+API_HASH = "1ea15c856ddfc5fa6b915f84e9156000"  # api hash
+
+BOT_TOKEN = "7112266365:AAHpEQ717YJc2wiF9hhDclQ2__kEHVhrD7w"  # bot token
+
+# REDIS
+HOST = "redis-18413.c330.asia-south1-1.gce.redns.redis-cloud.com"  # redis host uri
+PORT = 18413  # redis port
+PASSWORD = "GwcZfEoHzjOBvcWxAG1GkvtlGBNGNdEZ"  # redis password
+
+PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;"
+ADMINS = [5871400868]  # List of admin user IDs
+
+BOT_USERNAME = "teraboxfreedownloaderbot"  # Bot username
+
+# Force user to join this channel. (make sure you have promoted the bot on this chat.)
+FORCE_LINK = "https://t.me/+pj5f2vsuIwFiYWI1"
+
+PUBLIC_EARN_API = ""  # Your public earn API URL
